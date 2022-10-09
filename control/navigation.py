e519a4fe544112e833de23cd607a22e92036c3df
Vinicius <viniciusvvianna@gmail.com>
2022-10-08 23:29:11 -0300

    Updated style for add skills screen

--- control/navigation.py
+++ control/navigation.py
@@ -6,3 +6,14 @@ app = Flask(__name__, template_folder="../templates", static_folder="../static")
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/skills')
+def skills():
+    paths = {"ACE": "ACE", "AQU": "ÁQUILA", "ARA": "ARAUTO", "ARC": "ARCANTE", "BAR": "BARDO", "CHE": "CHEVALIER",
+             "CRU": "CRUZADO", "DRU": "DRUIDA", "ENC": "ENCANTADOR", "EVO": "EVOCADOR", "GUN": "GUNSLINGER",
+             "HAS": "HASSASSIN", "ILU": "ILUSIONISTA", "KAL": "KALINIER", "LAN": "LANCET", "MON": "MONGE",
+             "NEB": "NÉBULO", "PAL": "PALADINO", "PHA": "PHASER", "SHE": "SHEPIAN", "TEC": "TECNOCLASTA",
+             "TIM": "TIMESEER", "VAL": "VALETE", "VIB": "VIBRATA"}
+    categories = ['INTRINSECA', 'PADRAO', 'SUPORTE', 'REACAO', 'MOVIMENTO', 'PERFEITA']
+    return render_template('skills.html', categories=categories, paths=paths)
